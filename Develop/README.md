a0d964425aee48c40988f44bf8ad093f0c8044cf
jansferr <sferrazzajane@gmail.com>
2021-02-04 02:07:08 -0500

    Second pass at the Readme

--- Develop/README.md
+++ Develop/README.md
@@ -1,11 +1,44 @@
- # Sendme to Lee miles
- + ## Description Something good is going on
- + ## License 
- BSD
-
- # tell me
+ # jane
  + ## Description 
- alskjfjwoiajsnkjasdf
+ jane
+ 
+    + ##Table of Contents 
+  
+    + ##Installation 
+ al;skjdfjasf;asjdf
+ 
+     + ##Usage
+ aljsdjsadjsajfd 
+
+     + ##Credits
+ ;alskjljsljkl;aksjljsadf 
+
+     + ## License 
+ MIT
+ 
+ # TAKE ME TO LEE MILES
+ + ## Description 
+ This will be my path
+ + ##Table of Contents 
+  + ##Installation 
+ npm install
+ + ##Usage
+ image 
+ + ##Credits
+ James Michael 
  + ## License 
  MIT
-
+ 
+ # Your Project Title
+ + ## Description 
+ Your GitHub profile is an extremely important aspect of your public identity as a developer. A well-crafted one allows you to show off your work to other developers as well as potential employers. An important component of your GitHub profile—and one that many new developers often overlook—is the README.md file.
+ + ## Table of Contents 
+  + ## Installation 
+ There's no one right way to structure a good README. There is one very wrong way, however, and that is to not include a README at all or to create a very anemic one. This guide outlines a few best practices. As you progress in your career, you will develop your own ideas about what makes a good README.
+ + ## Usage
+ If you need an example of a good README, check out the VSCode repository.image 
+ + ## Credits
+ jerry mcguire 
+ + ## License 
+ MIT
+ 
--- Develop/generateReadme.js
+++ Develop/generateReadme.js
@@ -34,6 +34,25 @@ inquirer
 
     },
     {
+        type: 'input',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running. ',
+        name: 'installation',         
+        validate: (value) => { if(value){ return true} else{ return " I need a value to continue"}},
+       },
+       {
+        type: 'input',
+        message: 'Provide instructions and examples for use. Include screenshots as needed. ',
+        name: 'usage',         
+        validate: (value) => { if(value){ return true} else{ return " I need a value to continue"}},
+       },
+       //Credits : List your collaborators,
+       {
+        type: 'input',
+        message: 'List your collaborators ',
+        name: 'collaborators',         
+        validate: (value) => { if(value){ return true} else{ return " I need a value to continue"}},
+       },
+    {
         type: 'list',
         message: "What kind of License should your project have?",
         choices: ["BSD", "MIT", "GPL"],
@@ -41,9 +60,15 @@ inquirer
         validate: (value) => { if(value){ return true} else{ return " I need a value to continue"}},
 
     },
-   ])
+    //Badges: https://shields.io/
+    //Features If your project has a lot of features, consider adding a heading called "Features" and listing them there.
+    //Contributing: 
+    //If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The Contributor Covenant is an industry standard, but you can always write your own.
+    //Tests
+//Go the extra mile and write tests for your application. Then provide examples on how to run them.   
+])
    .then((response) => {
-    const newString = ` # ${response.project}\n + ## Description \n ${response.description}\n + ## License \n ${response.license}\n`;
+    const newString = ` # ${response.project}\n + ## Description \n ${response.description}\n + ## Table of Contents \n  + ## Installation \n ${response.installation}\n + ## Usage\n ${response.usage} \n + ## Credits\n ${response.collaborators} \n + ## License \n ${response.license}\n `;
     fs.appendFile('README.md', `${newString}\n`, (err) =>
     err ? console.error(err): console.log('Commit logged!')
     );
