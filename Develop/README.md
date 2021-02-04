d94812861898448af3ab5446277354bc7a3bb0fb
jansferr <sferrazzajane@gmail.com>
2021-02-04 01:30:58 -0500

    Readme file generated first draft

---
+++ Develop/README.md
@@ -0,0 +1,11 @@
+ # Sendme to Lee miles
+ + ## Description Something good is going on
+ + ## License 
+ BSD
+
+ # tell me
+ + ## Description 
+ alskjfjwoiajsnkjasdf
+ + ## License 
+ MIT
+
--- Develop/generateReadme.js
+++ Develop/generateReadme.js
@@ -3,47 +3,48 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 //inquirer to generate questions
-inquirer.prompt{
-   [
+inquirer
+    .prompt([
        {
-           type: 'input',
-           message = "What's the project title?",
-           name: "title",
-           validate: (value) => { if(value){ return true} else{ return " I need a value to continue"}}
+        type: 'input',
+        message: 'What is your Github username? ',
+        name: 'username',         
+        validate: (value) => { if(value){ return true} else{ return " I need a value to continue"}},
 
        },
        {
         type: 'input',
-        message = "What's the project title?",
-        name: "title",
+        message: 'What is your email address?',
+        name: 'email',
         validate: (value) => { if(value){ return true} else{ return " I need a value to continue"}}
 
     },
     {
         type: 'input',
-        message = "What's the project title?",
-        name: "title",
+        message: "What's the project's name?",
+        name: "project",
         validate: (value) => { if(value){ return true} else{ return " I need a value to continue"}}
 
     },
     {
         type: 'input',
-        message = "What's the project title?",
-        name: "title",
+        message: "Please write a short description of your project?",
+        name: "description",
         validate: (value) => { if(value){ return true} else{ return " I need a value to continue"}}
 
     },
     {
-        type: 'input',
-        message = "What's the project title?",
-        name: "title",
-        validate: (value) => { if(value){ return true} else{ return " I need a value to continue"}}
+        type: 'list',
+        message: "What kind of License should your project have?",
+        choices: ["BSD", "MIT", "GPL"],
+        name: "license",
+        validate: (value) => { if(value){ return true} else{ return " I need a value to continue"}},
 
-    }
-   ]
-   ).then({
-
-   })
-
-   ]
-}
+    },
+   ])
+   .then((response) => {
+    const newString = ` # ${response.project}\n + ## Description \n ${response.description}\n + ## License \n ${response.license}\n`;
+    fs.appendFile('README.md', `${newString}\n`, (err) =>
+    err ? console.error(err): console.log('Commit logged!')
+    );
+    });
